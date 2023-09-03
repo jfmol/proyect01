83045209dbfcd2540febe40375d4cd13e2c43577
jfmol <jfmolinos@hotmail.com>
2023-09-03 19:09:23 +0200

    mas variables en project

--- my_file_2.py
+++ my_file_2.py
@@ -1,7 +1,7 @@
-import my_file_1
+import my_file_1, my_file_3
 print('Hellow world')
 a= 5
 b = a+4
 c = a +b +54
 
-print(f"Mi variable es {b} + {c} + {my_file_1.z}")
+print(f"Mi variable es {b} + {c} + {my_file_1.z} +{my_file_3.j}")
